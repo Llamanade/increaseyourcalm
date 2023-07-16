1c2fadcb722962e3d5cd0efc5374c35dded98923
Llamanade <114692981+Llamanade@users.noreply.github.com>
2023-07-16 13:36:36 -0400

    Add files via upload

--- window size.js
+++ window size.js
@@ -1,8 +1,11 @@
+let width = window.innerWidth
+let height = window.innerHeight
+let x
+let nav = false
+
 setInterval(function myFunction(){
-    let width = window.innerWidth
-    let height = window.innerHeight
-    let x
-    let nav
+    width = window.innerWidth
+    height = window.innerHeight
 
     if (height >= width) {
         x = true
@@ -12,35 +15,37 @@ setInterval(function myFunction(){
     }
 
     if (x == true) {
-        document.body.style.setProperty('--fontSize', '2.8vw');
-        document.body.style.setProperty('--fontSizeHeading', '5vw');
-        document.body.style.setProperty('--fontSizeTitle', '10vw');
+        if (nav == false) {
+            document.body.style.setProperty('--fontSize', '2.8vw');
+            document.body.style.setProperty('--fontSizeHeading', '5vw');
+            document.body.style.setProperty('--fontSizeTitle', '10vw');
 
-        document.body.style.setProperty('--fontSizeOverlay', '1.5vw');
-        document.body.style.setProperty('--overlaySize', '23vw');
-        document.body.style.setProperty('--overlayTransform', '+1vw');
-        document.body.style.setProperty('--textTransform', '-24vw');
+            document.body.style.setProperty('--fontSizeOverlay', '1.4vw');
+            document.body.style.setProperty('--overlaySize', '22vw');
+            document.body.style.setProperty('--overlayTransform', '+1vw');
+            document.body.style.setProperty('--textTransform', '-24vw');
 
-        document.body.style.setProperty('--marginSmall', '5%');
+            document.body.style.setProperty('--marginSmall', '5%');
 
-        document.body.style.setProperty('--columnWidth', '25%');
+            document.body.style.setProperty('--columnWidth', '25%');
 
-        document.body.style.setProperty('--borderWidth', '1vw');
+            document.body.style.setProperty('--borderWidth', '1vw');
 
-        document.body.style.setProperty('--gap', '10%');
+            document.body.style.setProperty('--gap', '10%');
 
-        document.body.style.setProperty('--top', '5%');
+            document.body.style.setProperty('--top', '5%');
 
-        document.body.style.setProperty('--rows', '25% 25% 25%');
+            document.body.style.setProperty('--rows', '25% 25% 25%');
 
-        document.body.style.setProperty('--rowGap', '5%');
-        if (nav == true) {
+            document.body.style.setProperty('--rowGap', '5%');
+        }
+        else if (nav == true) {
             document.body.style.setProperty('--fontSize', '2.8vw');
             document.body.style.setProperty('--fontSizeHeading', '5vw');
             document.body.style.setProperty('--fontSizeTitle', '10vw');
 
             document.body.style.setProperty('--fontSizeOverlay', '1vw');
-            document.body.style.setProperty('--overlaySize', '18vw');
+            document.body.style.setProperty('--overlaySize', '17vw');
             document.body.style.setProperty('--overlayTransform', '.5vw');
             document.body.style.setProperty('--textTransform', '-18vw');
 
@@ -56,10 +61,10 @@ setInterval(function myFunction(){
         document.body.style.setProperty('--fontSizeHeading', '2.5vw');
         document.body.style.setProperty('--fontSizeTitle', '5vw');
 
-        document.body.style.setProperty('--fontSizeOverlay', '.75vw');
-        document.body.style.setProperty('--overlaySize', '11.5vw');
+        document.body.style.setProperty('--fontSizeOverlay', '.85vw');
+        document.body.style.setProperty('--overlaySize', '14vw');
         document.body.style.setProperty('--overlayTransform', '+.5vw');
-        document.body.style.setProperty('--textTransform', '-12vw');
+        document.body.style.setProperty('--textTransform', '-15vw');
 
         document.body.style.setProperty('--marginSmall', '2.5%');
 
@@ -79,10 +84,10 @@ setInterval(function myFunction(){
             document.body.style.setProperty('--fontSizeHeading', '2.5vw');
             document.body.style.setProperty('--fontSizeTitle', '5vw');
 
-            document.body.style.setProperty('--fontSizeOverlay', '.5vw');
-            document.body.style.setProperty('--overlaySize', '9vw');
-            document.body.style.setProperty('--overlayTransform', '.25vw');
-            document.body.style.setProperty('--textTransform', '-9vw');
+            document.body.style.setProperty('--fontSizeOverlay', '.64vw');
+            document.body.style.setProperty('--overlaySize', '10.5vw');
+            document.body.style.setProperty('--overlayTransform', '.4vw');
+            document.body.style.setProperty('--textTransform', '-11.5vw');
 
             document.body.style.setProperty('--marginSmall', '1.41%');
 
